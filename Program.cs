14431a151fbb4442e5b72b88fd7459212c7e78c2
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-14 16:21:09 +0530

    Create Binary Search Tree

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BinarySearchTree
+{
+    class Program
+    {
+        /// <summary>
+        /// ----------------->Welcome to Binary Search Tree<--------------------
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t----------->Welcome to Binary Search Tree<----------------");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            Console.WriteLine("InOrder Traversal For Given Is BST ");
+            binarySearch.Display();
+            Console.ReadLine();
+        }
+    }
+}
